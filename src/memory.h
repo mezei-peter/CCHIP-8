d201d3b549d3b6cb6e52a6064da349a939a51120
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-16 13:03:31 +0200

    Add space on the heap for fonts

--- src/memory.h
+++ src/memory.h
@@ -3,7 +3,8 @@
 #include <stdint.h>
 
 #define PROGRAM_ADDR 0x200
-#define HEAP_SIZE (4096 - PROGRAM_ADDR)
+#define FONTS_ADDR 0x050
+#define HEAP_SIZE 4096
 #define STACK_SIZE 16
 
 typedef struct Memory
