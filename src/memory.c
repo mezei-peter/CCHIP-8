fbc158cf0375e5dd1fef1dd6b4b198526e2021f3
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-16 12:28:17 +0200

    Construct Memory

--- src/memory.c
+++ src/memory.c
@@ -1,1 +1,15 @@
 #include "memory.h"
+
+Memory *mem_new()
+{
+    Memory *mem = malloc(sizeof(Memory));
+    for (int i = 0; i < sizeof(mem->heap) / sizeof(mem->heap[0]); i++)
+    {
+        mem->heap[i] = 0;
+    }
+    for (int i = 0; i < sizeof(mem->stack) / sizeof(mem->stack[0]); i++)
+    {
+        mem->stack[i] = 0;
+    }
+    return mem;
+}
--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <stdint.h>
+
+#define PROGRAM_ADDR 0x200
+#define HEAP_SIZE (4096 - PROGRAM_ADDR)
+#define STACK_SIZE 16
+
 typedef struct Memory
 {
-
+    uint8_t heap[HEAP_SIZE];
+    uint16_t stack[STACK_SIZE];
 } Memory;
+
+Memory *mem_new();
