7f1c77a6e86df4ee1be33d701d4e43f1227cb37a
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-23 15:42:02 +0200

    Add keys to Keypad

--- src/keypad.c
+++ src/keypad.c
@@ -5,6 +5,26 @@
 Keypad *keyp_new()
 {
     Keypad *keyp = malloc(sizeof(Keypad));
+    keyp->keys[0].scancode = SDL_SCANCODE_X;
+    keyp->keys[1].scancode = SDL_SCANCODE_1;
+    keyp->keys[2].scancode = SDL_SCANCODE_2;
+    keyp->keys[3].scancode = SDL_SCANCODE_3;
+    keyp->keys[4].scancode = SDL_SCANCODE_Q;
+    keyp->keys[5].scancode = SDL_SCANCODE_W;
+    keyp->keys[6].scancode = SDL_SCANCODE_E;
+    keyp->keys[7].scancode = SDL_SCANCODE_A;
+    keyp->keys[8].scancode = SDL_SCANCODE_S;
+    keyp->keys[9].scancode = SDL_SCANCODE_D;
+    keyp->keys[0xA].scancode = SDL_SCANCODE_Z;
+    keyp->keys[0xB].scancode = SDL_SCANCODE_C;
+    keyp->keys[0xC].scancode = SDL_SCANCODE_4;
+    keyp->keys[0xD].scancode = SDL_SCANCODE_R;
+    keyp->keys[0xE].scancode = SDL_SCANCODE_F;
+    keyp->keys[0xF].scancode = SDL_SCANCODE_V;
+    for (int i = 0; i < 16; i++)
+    {
+        keyp->keys[i].pressed = false;
+    }
     return keyp;
 }
 
--- src/keypad.h
+++ src/keypad.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+
+typedef struct Key
+{
+    SDL_Scancode scancode;
+    bool pressed;
+} Key;
+
 typedef struct Keypad
 {
-
+    Key keys[16];
 } Keypad;
 
 Keypad *keyp_new();
